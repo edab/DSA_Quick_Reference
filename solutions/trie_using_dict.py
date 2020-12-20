09f34c5bd174d2d312158419bf65b594f9246ce9
edab <ing.dabramo@gmail.com>
2020-12-20 21:42:09 +0100

    fix: improve unit test of trie

--- solutions/trie_using_dict.py
+++ solutions/trie_using_dict.py
@@ -2,49 +2,65 @@ from collections import defaultdict
 
 class TrieNode:
 
-  def __init__(self):
+    def __init__(self):
 
-    self.is_word = False
-    self.children = defaultdict(TrieNode)
-    
+        self.is_word = False
+        self.children = defaultdict(TrieNode)
+
+    def __str__(self, key="[root]", level=0):
+
+        ret = "  " * (level-1) + "+-" * (level > 0) + key + "\n"
+        for key in self.children:
+            ret += self.children[key].__str__(key, level + 1)
+        return ret
+
 class Trie:
 
-  def __init__(self):
+    def __init__(self, words = None):
 
-    self.root = TrieNode()
+        self.root = TrieNode()
 
-  def insert(self, word):
+        if words is not None:
+            for word in words:
+                self.insert(word)
 
-    curr_node = self.root
+    def insert(self, word):
 
-    for char in word:
-      curr_node = curr_node.children[char]
+        curr_node = self.root
 
-    curr_node.is_word = True
+        for char in word:
+            curr_node = curr_node.children[char]
 
-  def search(self, word):
-    
-    curr_node = self.root
+        curr_node.is_word = True
 
-    for char in word:
-      if char not in curr_node.children:
-        return False
-      curr_node = curr_node.children[char]
+    def search(self, word):
+      
+        curr_node = self.root
 
-    return curr_node.is_word
+        for char in word:
+            if char not in curr_node.children:
+                return False
+            curr_node = curr_node.children[char]
+
+        return curr_node.is_word
+
+    def __str__(self, level=0, node=None):
+        ret = "Trie\n"
+        ret += str(self.root)
+        return ret
+
 
 # Test cases
 word_list = ['apple', 'bear', 'goo', 'good', 'goodbye', 'goods', 'goodwill', 'gooses']
-word_trie = Trie()
+word_trie = Trie(word_list)
 
-# Add words
-for word in word_list:
-    word_trie.insert(word)
 
 # Test words
 test_words = ['bear', 'goo', 'good', 'goos']
-for word in test_words:
-    if word_trie.search(word):
-        print(f'"{word}" is a word.')
-    else:
-        print(f'"{word}" is not a word.')
+test_results = [True, True, True, False]
+
+print(word_trie)
+for word, res in zip(test_words, test_results):
+  pass_str = f"Pass [{word} is in trie]"
+  fail_str = f"Fail [{word} is in trie]"
+  print (pass_str if (word_trie.search(word) == res) else fail_str)
