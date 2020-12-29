450a02977543fb5a0953357780313a61fe1f7f14
edab <ing.dabramo@gmail.com>
2020-12-29 13:35:13 +0100

    fix: add bfs and print edge_list, adjacent_list and adjacent_matrix

--- docs/graphs.md
+++ docs/graphs.md
@@ -1,6 +1,6 @@
 # Graphs
 
-A **Graph** is an ordered pair of a set $V$ of vertices and a set $E$ of edges, and is a more generic data structure than a _tree_.
+A **Graph** is an ordered pair of a set $V$ of vertices and a set $E$ of edges and is a more generic data structure than a _tree_.
 
 <img src='https://g.gravizo.com/svg?
   digraph G {
@@ -10,38 +10,38 @@ A **Graph** is an ordered pair of a set $V$ of vertices and a set $E$ of edges, 
   }
 '/>
 
-Graphs can be **undirected**, when each edge can be traversed in both directions, or **directed**, where an edge can be traversed only in one direction and is represented with a arrow.
+Graphs can be **undirected**, when each edge can be traversed in both directions, or **directed**, where an edge can be traversed only in one direction and is represented with an arrow.
 
-Graphs can be **weighted**, where a number is associated to each edge, that can be for example the cost of traversing that specific edge.
+Graphs can be **weighted**, where a number is associated with each edge, that can be for example the cost of traversing that specific edge.
 
-A **disconnected** graph will contain nodes that cannot be reached from some other nodes, and in a **fully connected** graph there is at least one edge for each node present.
+A **disconnected** graph will contain nodes that cannot be reached from some other nodes, and in a **fully connected** graph, there is at least one edge for each node present.
 
 ## Representation
 
 Graphs can be represented in different ways, each one with its pros and cons.
 
-- **Vertex object**: an object that represent a node, with an attribute like an edges list that contain all the other nodes connected.
-- **Edge object**: an object that represent an edge, with an attribute like a node list that contain the two nodes connected by the edge.
+- **Vertex object**: an object that represents a node, with an attribute like an edges list that contains all the other nodes connected.
+- **Edge object**: an object that represents an edge, with an attribute like a node list that contains the two nodes connected by the edge.
 - **Edge list**: a 2D list of edges represented by lists of two elements, the starting node and the ending node.
 - **Adjacency list**: a 2D list of nodes, where at each index of the list there is a list of adjacent nodes.
-- **Adjacent matrix**: each row and column index represent a source and destination node, and a value of 1 in the matrix indicates that the node are connected.
+- **Adjacent matrix**: each row and column index represent a source and a destination node, and a value of 1 in the matrix indicates that the nodes are connected.
 
 ## Terminology
 
 There is some specific terminology associated with the graph:
 
-- **Node** or **Vertex**: the basic element of a graph containing data.
+- **Node** or **Vertex**: the basic elements of a graph containing data.
 - **Edge**: a connection between two vertices.
 - **Loop**: when an edge from a node is incident on itself.
 - **Degree of a vertex**: number of vertices that are incident on a given vertex.
-- **Adjacency**: connection of a node to its neighbor.
-- **Path**: sequence of vertices connected by edges.
+- **Adjacency**: connection of a node to its neighbour.
+- **Path**: a sequence of vertices connected by edges.
 
 ## Operations
 
 ### Traversal
 
-There are two fundamental technique for graph traversal:
+There are two fundamental techniques for graph traversal:
 
 - **Depth First Search (DFC)**: where we follow one path as far as it will go.
 - **Breadth First Search (BFS)**: where we look at all the node adjacent to one before moving on to the next level.
@@ -52,19 +52,31 @@ Both techniques are often used as a basis for developing more advanced algorithm
 
 ### Basic Graph Representations
 
-### Depth First Search
+### Traversing
 
-_DFS_ implementation is very similar to the one already seen on _Tree_, but since there is not root node, there is no obvious place to start.
+_DFS_ and _BFS_ implementation are very similar to the one for _Trees_, but since there is no root node, there is no obvious place to start.
 
-Common implementation of _DFS_ uses stack, and repeat the following operations:
+#### Depth First Search
 
-- we sign the node as visited
-- we add the node to the stack
-- we peek an edge, and follow it to a node, and mark it as seen and add it to the stack
-- as long as there are more edge and unseen nodes, we repeat that step
-- when we hit a node already seen, we go back and try another edge
-- if we finish the edge, we pop a node from the stack and continue from there
+The implementation of _DFS_ is based on a _stack_, and repeat the following operations:
 
-The runtime complexity is $O(|V| + |E|)$, where $V$ is the number of vertex and $E$ is the number of edges.
+1. Start from a node and sign as visited
+2. Add the node to the stack
+3. Peek an edge, follow it to a node, and mark it as seen and add it to the stack
+4. as long as there are more edge and unseen nodes, we repeat that step
+5. when we hit a node already seen, we go back and try another edge
+6. if we finish the edge, we pop a node from the stack and continue from there
 
-Technically we visit each edge twice, one for explore and one for traveling back through it.
+The runtime complexity is $O(|V| + |E|)$, where $V$ is the number of vertexes and $E$ is the number of edges.
+
+Technically we visit each edge twice, one for explore and one for travelling back through it.
+
+### Breadth First Search
+
+The implementation of _BFS_ is based on a _queue_, and repeat the following operations:
+
+1. Start from a node and sign as visited
+2. Visit all adjacent nodes to it, and add it to the queue
+3. We go back to the first node, visit all its adjacent nodes and add it to the queue
+
+We can think on _BFS_ as we are constructing a tree, were the child of a node are all its adjacent nodes, and where we proceed to visit by levels.
--- solutions/graphs.py
+++ solutions/graphs.py
@@ -1,25 +1,98 @@
 class GraphNode(object):
+
     def __init__(self, val):
         self.value = val
         self.children = []
         
-    def add_child(self,new_node):
+    def add_child(self, new_node):
         self.children.append(new_node)
     
-    def remove_child(self,del_node):
+    def remove_child(self, del_node):
         if del_node in self.children:
             self.children.remove(del_node)
 
 class Graph(object):
-    def __init__(self,node_list):
+
+    def __init__(self, node_list):
         self.nodes = node_list
+
+    def edge_list(self):
+
+        edge_list = list()  # set give better result, but we use here 2D list
+        visited = set()
+        queue = [self.nodes[0]]
         
-    def add_edge(self,node1,node2):
+        # Visit graph using BFS
+        while len(queue) > 0:
+
+            current_node = queue.pop(0)
+            visited.add(current_node)
+
+            for child in current_node.children:
+                # Since is an undirected graph, we check both ways
+                source = current_node.value
+                dest = child.value
+                if [source, dest] not in edge_list and [dest, source] not in edge_list:
+                    edge_list.append([source, dest])
+                if child not in visited:
+                    queue.append(child)
+
+        return edge_list
+
+    def adjacent_list(self):
+
+        adjacent_list = list()
+        visited = set()
+        queue = [self.nodes[0]]
+        
+        # Visit graph using BFS
+        while len(queue) > 0:
+
+            current_node = queue.pop(0)
+            visited.add(current_node)
+
+            node_adjacent_list = list()
+
+            for child in current_node.children:
+                node_adjacent_list.append(child.value)
+                if child not in visited:
+                    queue.append(child)
+
+            adjacent_list.append(node_adjacent_list)
+
+        return adjacent_list
+
+    def adjacent_matrix(self):
+
+        node_list = [node.value for node in self.nodes]
+        node_list.sort()
+        matrix = list([[0 for x in range(len(node_list))] for x in range(len(node_list))])
+        visited = set()
+        queue = [self.nodes[0]]
+        
+        # Visit graph using BFS
+        while len(queue) > 0:
+
+            current_node = queue.pop(0)
+            visited.add(current_node)
+
+            for child in current_node.children:
+                # Since is an undirected graph, we check both ways
+                source_idx = node_list.index(current_node.value)
+                dest_idx = node_list.index(child.value)
+                matrix[source_idx][dest_idx] = 1
+                matrix[dest_idx][source_idx] = 1
+                if child not in visited:
+                    queue.append(child)
+
+        return node_list, matrix
+        
+    def add_edge(self, node1, node2):
         if(node1 in self.nodes and node2 in self.nodes):
             node1.add_child(node2)
             node2.add_child(node1)
             
-    def remove_edge(self,node1,node2):
+    def remove_edge(self, node1, node2):
         if(node1 in self.nodes and node2 in self.nodes):
             node1.remove_child(node2)
             node2.remove_child(node1)
@@ -27,8 +100,9 @@ class Graph(object):
 
     def dfs_search(self, root_node, search_value):
 
-        visited = set()                         # Sets are faster for lookups
-        
+        # Sets are faster for lookups
+        visited = set()          
+
         # Start with a given root node
         stack = [root_node]                     
         
@@ -51,7 +125,6 @@ class Graph(object):
                 if (child not in visited) and (child not in stack):         
                     stack.append(child)
 
-    # Solution
     def dfs_search_recursive(self, start_node, search_value):
 
         # Set to keep track of visited nodes
@@ -79,7 +152,40 @@ class Graph(object):
                     break
 
         return result
-                
+
+    def bfs_search(self, root_node, search_value):
+
+        # Sets are faster for lookups
+        visited = set()
+        queue = [root_node]
+        
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            visited.add(current_node)
+
+            if current_node.value == search_value:
+                return current_node
+
+            for child in current_node.children:
+                if child not in visited:
+                    queue.append(child)
+
+# Helper functions
+def print_edge(edge_list):
+    for edge in edge_list:
+        print(f"  - {edge}")
+
+def print_adjacent_list(adjacent_list):
+    for neighbour in adjacent_list:
+        print(f"  - {neighbour}")
+
+def print_adjacent_matrix(nodes, matrix):
+    # Print column headers
+    print("    " + ' '.join(map(str, nodes)))
+    index = 0
+    for row in matrix:
+        print(f"  {nodes[index]} " + ' '.join(map(str, row)))
+        index += 1
 
 # Test Cases
 nodeG = GraphNode('G')
@@ -98,19 +204,36 @@ graph1.add_edge(nodeH,nodeG)
 graph1.add_edge(nodeH,nodeP)
 graph1.add_edge(nodeS,nodeR)
 
-# To verify the graph, we print all the parent nodes and child nodes
-for each in graph1.nodes:
-    print('parent node = ',each.value,end='\nchildren\n')
-    for each in each.children:
-        print(each.value,end=' ')
-    print('\n')
-
+# DFS Tests
+print("DFS")
 print("  Iterative version") 
-print("     Pass" if (graph1.dfs_search(nodeS, 'A') == nodeA) else "     Fail")
-print("     Pass" if (graph1.dfs_search(nodeS, 'S') == nodeS) else "     Fail")
-print("     Pass" if (graph1.dfs_search(nodeS, 'R') == nodeR) else "     Fail")
+print("     Search A from S: " + "Pass" if (graph1.dfs_search(nodeS, 'A') == nodeA) else "     Fail")
+print("     Search S from S: " + "Pass" if (graph1.dfs_search(nodeS, 'S') == nodeS) else "     Fail")
+print("     Search R from S: " + "Pass" if (graph1.dfs_search(nodeS, 'R') == nodeR) else "     Fail")
 print("  Recoursive version") 
-print("     Pass" if (graph1.dfs_search_recursive(nodeG, 'A') == nodeA) else "     Fail")
-print("     Pass" if (graph1.dfs_search_recursive(nodeS, 'A') == nodeA) else "     Fail")
-print("     Pass" if (graph1.dfs_search_recursive(nodeP, 'S') == nodeS) else "     Fail")
-print("     Pass" if (graph1.dfs_search_recursive(nodeH, 'R') == nodeR) else "     Fail")
+print("     Search A from G: " + "Pass" if (graph1.dfs_search_recursive(nodeG, 'A') == nodeA) else "     Fail")
+print("     Search A from S: " + "Pass" if (graph1.dfs_search_recursive(nodeS, 'A') == nodeA) else "     Fail")
+print("     Search S from P: " + "Pass" if (graph1.dfs_search_recursive(nodeP, 'S') == nodeS) else "     Fail")
+print("     Search R from H: " + "Pass" if (graph1.dfs_search_recursive(nodeH, 'R') == nodeR) else "     Fail")
+
+# BFS Tests
+print("BFS")
+print("     Search A from S: " + "Pass" if (graph1.bfs_search(nodeS, 'A') == nodeA) else "     Fail")
+print("     Search S from P: " + "Pass" if (graph1.bfs_search(nodeP, 'S') == nodeS) else "     Fail")
+print("     Search R from H: " + "Pass" if (graph1.bfs_search(nodeH, 'R') == nodeR) else "     Fail")
+
+# Edge list tests
+print("Edge list representation")
+#print_edge(graph1.edge_list())
+print("     Pass" if (graph1.edge_list() == [['S', 'R'], ['R', 'G'], ['R', 'A'], ['R', 'P'], ['G', 'A'], ['G', 'H'], ['P', 'H']]) else "     Fail")
+
+# Adjacent list tests
+print("Adjacent list representation")
+#print_adjacent_list(graph1.adjacent_list())
+print("     Pass" if (graph1.adjacent_list() == [['R'], ['G', 'A', 'P', 'S'], ['R', 'A', 'H'], ['R', 'G'], ['R', 'H'], ['R', 'G'], ['G', 'P'], ['G', 'P']]) else "     Fail")
+
+# Adjacent matrix tests
+print("Adjacent matrix representation")
+nodes, matrix = graph1.adjacent_matrix()
+#print_adjacent_matrix(nodes, matrix)
+print("     Pass" if (matrix == [[0, 1, 0, 0, 1, 0], [1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 0], [0, 0, 1, 0, 1, 0], [1, 1, 0, 1, 0, 1], [0, 0, 0, 0, 1, 0]]) else "     Fail")
