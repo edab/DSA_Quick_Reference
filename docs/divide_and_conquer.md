e203b8216d54a36a18326e644f781abe7bcd2827
edab <ing.dabramo@gmail.com>
2021-01-24 23:03:22 +0100

    fix: divide and conquer example

--- docs/divide_and_conquer.md
+++ docs/divide_and_conquer.md
@@ -15,6 +15,8 @@ The use of _recursion_ make the code harder to debug and requires large stacks, 
 
 ## Median Problem
 
+Python3 implementation: [dac_kth_largest_element.py](../solutions/dac_kth_largest_element.py)
+
 The _Median Problem_ can be a perfect example for this solving technique, and can be defined as: given an unsorted list `A=[a_1, a_2, ..., a_n]` of n elements, found the median of A.
 
 We define a _median_ element as the $[n/2]^{th}$ element of the list, and we focus on the solution a more general problem, to find the $k^{th}$ element, where `k` is a number between 1 and n.
@@ -23,10 +25,25 @@ The first solution could be to first sort the algorithm, and then directly get t
 
 But there is a better solution, know as [Median of medians](http://people.csail.mit.edu/rivest/pubs/BFPRT73.pdf), thanks to Blum, Floyd, Pratt,. Rivest, Tarjan (1973).
 
-The solution remind the _Quicksort_ algorithm, that works as follow:
-1. A _pivot_ `p` is chosen.
-2. The array is partitioned into three bucket, `A<p`, `A=p`, `A>p`.
+The solution architecture remind the _Quicksort_ algorithm, that works as follow:
+
+1. We choose a _pivot_ `p`.
+2. We move all the array element into three bucket, `A<p`, `A=p`, `A>p`.
 3. The algorithm is recursively applied to `A<p` and the `A>p`.
 4. The final output will be the sorted list of `A<p`, then the `A=p` element and `A>p`.
 
 If we chose the wrong _pivot_, like the maximum or minimum element, the time complexity of the quicksort will be $O(N^2)$.
+
+The _perfect pivot_ would be the median of A, but since is the ones we are searching for, we target a _good pivot_, that appears in the region from 1/4 to 3/4 of the sorted array A.
+
+### Fast median solution
+
+A nice solution to the problem can be:
+1. Break A in 5 groups of $n/5$ elements each (one swipe of the array, $O(N)$).
+2. For each group, sort the group and find the median of each group (only sort 5 element, $O(1)$ for each group, $O(N)$).
+3. Create a list S of median of all groups.
+4. Run the same first three steps on array S of length $n/5$ ($O(N/5)$).
+5. Partition the array A into three bucket, $A<p$, $A=p$, $A>p$ (one swipe of the array, $O(N)$).
+6. Recourse on $A<p$, $A>p$ or output p depending on k value ($O(3n/4)$, since p is a good pivot, and the two bucket have a maximum size of $3n/4$).
+
+So, with this algorithm, we have a time complexity of $O(N)$, that is a great improvement over the one of the simple solution of sorting first, and the select the key.
---
+++ solutions/dac_kth_largest_element.py
@@ -0,0 +1,101 @@
+# Divide and Conquer
+#
+# Problem: Given an unsorted array Arr with n positive integers, find the k_th 
+#          smallest element in the given array.
+#   Input: Unsorted array Arr of length n and an integer k where 1≤k≤n
+#  Output: The k_th smallest element of array Arr
+#
+# Example 1
+#   Arr = [6, 80, 36, 8, 23, 7, 10, 12, 42, 99]
+#   k = 10
+#   Output = 99
+#
+# Example 2
+#   Arr = [6, 80, 36, 8, 23, 7, 10, 12, 42, 99]
+#   k = 5
+#   Output = 12
+from typing import List
+
+def findMedian(Arr: List[int], start: int, size: int) -> int: 
+    '''
+    Helper function for find the median in a list, sorting it
+    '''
+
+    # Get the slice of interest
+    if start + size > len(Arr):
+        size = len(Arr) - start 
+    
+    myList = Arr[start:start+size] 
+          
+    # Sort the array  
+    myList.sort() 
+  
+    # Return the middle element 
+    return myList[size // 2] 
+
+def fastSelect(Arr: List[int], k: int) -> int:
+    '''
+    Find the k_th smallest element in the array Arr
+    '''
+
+    n = len(Arr)                                
+    
+    # Base check
+    if (k > 0 and k <= n):
+
+        medians = []
+        arr_less_p = []
+        arr_equal_p = []
+        arr_more_p = []
+        i = 0
+        
+        # Step 1 - Break Arr into groups of size 5
+        # Step 2 - For each group, sort and find median, add it to the medians
+        #while (i < n // 5):
+        while (i < n):
+            median = findMedian(Arr, i, 5)
+            medians.append(median)         
+            i += 5
+        
+        # Step 3 - Find the median of medians
+        if (len(medians) == 1):
+            pivot = medians[0]
+        elif (len(medians) > 1):
+            pivot = fastSelect(medians, (len(medians)//2))
+        
+        # Step 4 - Partition the original Arr into three sub-arrays
+        for element in Arr:
+            if (element<pivot):
+                arr_less_p.append(element)
+            elif (element>pivot):
+                arr_more_p.append(element)
+            else:
+                arr_equal_p.append(element)
+        
+        # Step 5 - Recurse based on the sizes of the three sub-arrays
+        if (k <= len(arr_less_p)):
+            return fastSelect(arr_less_p, k)
+        
+        elif (k > (len(arr_less_p) + len(arr_equal_p))):
+            return fastSelect(arr_more_p, (k - len(arr_less_p) - len(arr_equal_p)))
+            
+        else:
+            return pivot     
+
+# Test cases
+print('Testing Kth Largest Element solution')
+
+# Test 1
+Arr = [6, 80, 36, 8, 23, 7, 10, 12, 42]
+k = 5
+print('  Test 1: ' + 'Pass' if fastSelect(Arr, k) == 12 else 'Fail' )
+
+# Test 2
+Arr = [5, 2, 20, 17, 11, 13, 8, 9, 11]
+k = 5
+print('  Test 2: ' + 'Pass' if fastSelect(Arr, k) == 11 else 'Fail' )
+
+# Test 3
+Arr = [6, 80, 36, 8, 23, 7, 10, 12, 42, 99]
+k = 10
+print('  Test 3: ' + 'Pass' if fastSelect(Arr, k) == 99 else 'Fail' )
